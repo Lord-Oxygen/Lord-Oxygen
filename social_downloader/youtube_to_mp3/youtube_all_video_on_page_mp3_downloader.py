aae1062dd0f49c42d6d27e6bf4d13d5dd413a5f8
LordOxygen <dydy60717@gmail.com>
2024-06-03 01:30:17 +0200

    commit

---
+++ social_downloader/youtube_to_mp3/youtube_all_video_on_page_mp3_downloader.py
@@ -0,0 +1,39 @@
+#youtube_all_video_on_page_mp3_downloader https://www.youtube.com/@JadaFacer/videos
+
+# import HTMLSession from requests_html
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+from youtube_to_mp3 import download_mp3
+
+
+
+def get_all_the_links(url):
+	not_done = True
+	while not_done:
+		print("|", end="")
+		# create an HTML Session object
+		session = HTMLSession()
+		 
+		# Use the object above to connect to needed webpage
+		resp = session.get(url)
+		 
+		# Run JavaScript code on webpage
+		resp.html.render()
+		html_page = resp.html.html
+		link_dict = {}# {/watch?v=asdh12 : True}
+		soup = BeautifulSoup(html_page, features="lxml")
+		for link in soup.findAll('a'):
+			watch_link = str(link.get('href'))
+			if watch_link.startswith("/watch"):
+				link_dict["https://www.youtube.com"+watch_link] = True
+				if len(link_dict) > 2:
+					not_done = False
+	
+	link_list = []
+	for link in link_dict.keys(): link_list.append(link)
+	return link_list
+if __name__ == '__main__':
+	url = input("Enter the URL of the page with the videos you want to download\n>>")
+	link_list = get_all_the_links(url)
+	for link in link_list:
+		download_mp3(link)
---
+++ social_downloader/youtube_to_mp3/youtube_to_mp3.py
@@ -0,0 +1,35 @@
+# importing packages 
+from pytube import YouTube 
+import os 
+import sys
+def download_mp3(input_):
+    while True:
+        # url input from user#
+        
+        yt = YouTube(str(input_)) 
+          
+        # extract only audio 
+        video = yt.streams.filter(only_audio=True).first() 
+          
+        # check for destination to save file 
+        destination = "downloads/"
+          
+        # download the file 
+        out_file = video.download(output_path=destination) 
+          
+        # save the file 
+        base, ext = os.path.splitext(out_file) 
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file) 
+          
+        # result of success 
+        print(yt.title + " has been successfully downloaded.")
+    
+        if input_ == "exit":
+            sys.exit()
+        if not __name__ == '__main__':
+            break
+
+if __name__ == '__main__':
+        input_ = input("Enter the URL of the video you want to download (or type exit to exit): \n>> ")
+        download_mp3(input_)
